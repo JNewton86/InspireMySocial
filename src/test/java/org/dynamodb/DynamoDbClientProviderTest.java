91de6a62988c3240db0c75cc48cfc3a5e7e53ec7
Jeff Newton <jeff.newton86@live.com>
2023-05-17 09:54:03 -0500

    reorg of files and removed example code from gradle build

---
+++ src/test/java/org/dynamodb/DynamoDbClientProviderTest.java
@@ -0,0 +1,14 @@
+package org.dynamodb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DynamoDbClientProviderTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
